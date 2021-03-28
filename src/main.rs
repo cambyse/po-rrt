d0e4d82a5928e022a3757589be6dbc55eaf58515
Camille Phiquepal <camille.phiquepal@gmail.com>
2021-03-28 13:39:18 +0200

    dead code

--- src/main.rs
+++ src/main.rs
@@ -129,27 +129,6 @@ fn display(){
 }
 
 fn main() {
-    /*
-	let mut m = Map::open("data/map2.pgm", [-1.0, -1.0], [1.0, 1.0]);
-	m.add_zones("data/map2_zone_ids.pgm", 0.2);
-
-	fn goal(state: &[f64; 2]) -> WorldMask {
-		bitvec![if (state[0] - 0.55).abs() < 0.05 && (state[1] - 0.9).abs() < 0.05 { 1 } else { 0 }; 4]
-	}
-
-	let mut prm = PRM::new(ContinuousSampler::new([-1.0, -1.0], [1.0, 1.0]),
-						   DiscreteSampler::new(),
-						   &m);
-
-	prm.grow_graph(&[0.55, -0.8], goal, 0.05, 5.0, 5000, 100000).expect("graph not grown up to solution");
-	let policy = prm.plan_belief_state(&vec![0.2, 0.2, 0.2, 0.4]);
-
-	let mut m2 = m.clone();
-	m2.resize(5);
-	m2.draw_full_graph(&prm.graph);
-	m2.draw_policy(&policy);
-    m2.save("results/test_plan_on_map2_pomdp_main.pgm");*/
-    
     let mut m = Map::open("data/map4.pgm", [-1.0, -1.0], [1.0, 1.0]);
 	m.add_zones("data/map4_zone_ids.pgm", 0.15);
 
